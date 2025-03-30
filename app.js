2b2b93def3d10393e9b1cd49c60c494b22b8b84f
dk-willing <davidkarikari89@gmail.com>
2025-03-30 16:18:34 +0000

    Made navbar fixed

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ const contactModalCloseBtn = document.querySelectorAll(".fa-xmark");
 const contactModalForm = document.querySelector(".contact__modal-form");
 const successMessage = document.querySelector(".success");
 
+const navbar = document.querySelector(".header__nav");
+
+// Listening to scroll effect on the windows
+window.addEventListener("scroll", () => {
+  if (scrollY) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
 contactElement.forEach((item) => {
   item.addEventListener("click", (e) => {
     e.preventDefault();
